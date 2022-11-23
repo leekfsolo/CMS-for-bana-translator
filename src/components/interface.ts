40efbed8ff9faaf2a39a8487a5a75390aff07536
changngoc29 <vtkienn@gmail.com>
2022-11-23 16:48:48 +0700

    add profile page and make component of cform

--- src/components/interface.ts
+++ src/components/interface.ts
@@ -1,6 +1,35 @@
+import {HTMLInputTypeAttribute} from 'react';
+
 export interface ISidebarItem {
   label: string;
   src: string;
   icon?: React.ReactNode;
   subItems?: ISidebarItem[];
+}
+
+export interface ICForm {
+  username?: string;
+  fullname?: string;
+  phone?: string;
+  password?: string;
+  confirmPassword?: string;
+  accountRole?: string;
+  currPassword?: string;
+  newPassword?: string;
+  confirmNewPassword?: '';
+}
+
+export interface ICFormInput {
+  label: string;
+  required: boolean;
+  type: HTMLInputTypeAttribute;
+  placeholder: string;
+  id: string;
+  options?: string[];
+  name: keyof ICForm;
+}
+
+export interface ICFormSection {
+  title: string;
+  items: ICFormInput[];
 }
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,135 @@
 import React from 'react';
+import {IProfileSection, IProfile} from 'pages/interface';
+import {Button} from '@mui/material';
+import {useState} from 'react';
+import {ICForm, ICFormSection} from 'components/interface';
+import CForm from 'components/CForm';
+
+const userData: IProfile = {
+  username: 'username',
+  password: '**********',
+  fullname: 'Nguyen Van A',
+  phone: '0911 011 011',
+  accountRole: 'sub-admin'
+};
+
+const defaultValues: ICForm = {
+  currPassword: '',
+  newPassword: '',
+  confirmNewPassword: ''
+};
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const {username, password, fullname, phone, accountRole}: IProfile = userData;
+  const profileSections: IProfileSection[] = [
+    {
+      title: 'Profile',
+      items: [
+        {
+          key: 'Fullname',
+          value: fullname
+        },
+        {
+          key: 'Phone',
+          value: phone
+        },
+        {
+          key: 'Account role',
+          value: accountRole
+        }
+      ]
+    },
+    {
+      title: 'Authentication',
+      items: [
+        {
+          key: 'username',
+          value: username
+        },
+        {
+          key: 'password',
+          value: password
+        }
+      ]
+    }
+  ];
+  const changePasswordFormSections: ICFormSection[] = [
+    {
+      title: 'Change Password',
+      items: [
+        {
+          label: 'Current Password',
+          id: 'curr-password',
+          name: 'currPassword',
+          required: true,
+          placeholder: 'Enter current password',
+          type: 'password'
+        },
+        {
+          label: 'New Password',
+          id: 'new-password',
+          name: 'newPassword',
+          required: true,
+          placeholder: 'Enter new password',
+          type: 'password'
+        },
+        {
+          label: 'Confirm New Password',
+          id: 'confirm-new-password',
+          name: 'confirmNewPassword',
+          required: true,
+          placeholder: 'Confirm new password',
+          type: 'password'
+        }
+      ]
+    }
+  ];
+  const [isChangePassword, setIsChangePassword] = useState(false);
+  return (
+    <div className='profile'>
+      {profileSections.map((section) => (
+        <div className='profile-section' key={section.title}>
+          <div className='profile-section__title'>
+            <h4>{section.title}</h4>
+          </div>
+
+          <div className='profile-section__infos'>
+            {section.items.map((item, index) => {
+              return (
+                <div key={index} className='info-row'>
+                  <div className='info-row__key'>
+                    <p>{item.key}</p>
+                  </div>
+
+                  <div className='info-row__value'>
+                    <p>{item.value}</p>
+                  </div>
+
+                  {item.key === 'password' ? (
+                    <div className='changePassword-button'>
+                      <Button
+                        size='small'
+                        variant='contained'
+                        onClick={() => {
+                          setIsChangePassword(!isChangePassword);
+                        }}
+                      >
+                        change password
+                      </Button>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ))}
+
+      {isChangePassword ? <CForm sections={changePasswordFormSections} defaultValues={defaultValues} /> : <></>}
+    </div>
+  );
 };
 
 export default Profile;
