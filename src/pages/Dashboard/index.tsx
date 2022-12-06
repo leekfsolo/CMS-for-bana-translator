447d6ef6bae8a589cb5eaffb1266cfbcb72e59a6
Ken <gialap2002@gmail.com>
2022-12-06 18:04:24 +0700

    add dashboard card

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,7 +3,10 @@ import CTable from 'components/CTable';
 import CTableToolbar from 'components/CTableToolbar';
 import {Paper} from '@mui/material';
 import {ITrainingHistory, TableHeadCell} from 'pages/interface';
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import TranslateIcon from '@mui/icons-material/Translate';
+import CampaignIcon from '@mui/icons-material/Campaign';
+import QueueIcon from '@mui/icons-material/Queue';
 
 function createData(
   user: string,
@@ -78,12 +81,79 @@ const Dashboard = () => {
   const [selected, setSelected] = useState<string[]>([]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const cardProgressInnerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (cardProgressInnerRef.current) {
+        cardProgressInnerRef.current.style.width = '50%';
+      }
+    }, 200);
+  }, []);
 
   return (
     <div className='dashboard'>
-      <div className='dashboard-overview w-100 d-flex gap-3 justify-content-between mb-4'>
-        <div className='dashboard-overview__card'></div>
-        <div className='dashboard-overview__card'></div>
+      <div className='dashboard-overview w-100 d-flex flex-wrap gap-3 justify-content-between mb-3'>
+        <div className='dashboard-overview__card card-translate'>
+          <div className='card-info d-flex align-items-center mb-3'>
+            <p className='m-0 pe-1'>NMT</p>
+            <span className='ps-1 card-version'>1.0.1</span>
+          </div>
+
+          <div className='card-icon d-flex align-items-center gap-3'>
+            <div className='card-icon__svg'>
+              <TranslateIcon />
+            </div>
+
+            <div className='card-extend'>
+              <p className='card-extend__accuracy mb-1'>
+                Accuracy: <span>93.8%</span>
+              </p>
+              <p className='card-extend__date'>
+                Training date: <span>10/21/2022</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='dashboard-overview__card card-speech'>
+          <div className='card-info d-flex align-items-center mb-3'>
+            <p className='m-0 pe-1'>TTS</p>
+            <span className='ps-1 card-version'>1.0.1</span>
+          </div>
+
+          <div className='card-icon d-flex align-items-center gap-3'>
+            <div className='card-icon__svg'>
+              <CampaignIcon />
+            </div>
+
+            <div className='card-extend'>
+              <p className='card-extend__accuracy mb-1'>
+                Accuracy: <span>93.8%</span>
+              </p>
+              <p className='card-extend__date'>
+                Training date: <span>10/21/2022</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='dashboard-overview__card card-queue'>
+          <div className='card-info d-flex align-items-center mb-3 justify-content-between'>
+            <p className='m-0 pe-1'>Training Queue</p>
+            <div className='queue-count'>
+              <span className='current-count'>4</span>
+              <span className='max-count'>/8</span>
+            </div>
+          </div>
+
+          <div className='card-icon d-flex align-items-center gap-4'>
+            <div className='card-icon__svg'>
+              <QueueIcon />
+            </div>
+            <div className='card-progress'>
+              <div className='progress-inner' ref={cardProgressInnerRef}></div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className='dashboard-training'>
         <Paper sx={{width: '100%', mb: 2}}>
